afb0dd752c721d2000da574d2a7bc44e07090505
evf <eliseuv816@gmail.com>
2022-06-27 14:58:21 -0300

    update

--- emacs/.config/doom/custom.el
+++ emacs/.config/doom/custom.el
@@ -6,7 +6,9 @@
  '(conda-anaconda-home "/opt/anaconda")
  '(custom-safe-themes
    '("234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" default))
- '(package-selected-packages '(latex-preview-pane irony)))
+ '(org-agenda-files
+   '("/home/evf/programs/phd/papers/brass_ca_magnet_ts/notes.org" "/home/evf/Documents/org/agenda.org" "/home/evf/Documents/org/elfeed.org" "/home/evf/Documents/org/masters.org" "/home/evf/Documents/org/notes.org" "/home/evf/Documents/org/phd.org" "/home/evf/Documents/org/reading_list.org" "/home/evf/Documents/org/todo.org"))
+ '(package-selected-packages '(gnu-apl-mode latex-preview-pane irony)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
