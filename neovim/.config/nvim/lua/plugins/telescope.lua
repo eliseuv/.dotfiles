bd6d09045147774dc2357665dfb81e6d1ac675eb
evf <eliseuv816@gmail.com>
2023-02-21 20:20:56 -0300

    update

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,17 +1,24 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  keys = {
-    -- -- disable the keymap to grep files
-    -- {"<leader>/", false},
-    -- -- change a keymap
-    -- { "<leader>ff", "Telescope find_files" desc = "Find Files" },
-    -- add a keymap to browse plugin files
-    {
-      "<leader>fp",
-      function()
-        require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
-      end,
-      desc = "Find Plugin File",
+    "nvim-telescope/telescope.nvim",
+    opts = {
+        pickers = {
+            find_files = {
+                find_command = { "rg", "--ignore", "--follow", "--hidden", "--files" },
+            },
+        },
     },
-  },
+    keys = {
+        -- -- disable the keymap to grep files
+        -- {"<leader>/", false},
+        -- -- change a keymap
+        -- { "<leader>ff", "Telescope find_files" desc = "Find Files" },
+        -- add a keymap to browse plugin files
+        {
+            "<leader>fp",
+            function()
+                require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+            end,
+            desc = "Find Plugin File",
+        },
+    },
 }
