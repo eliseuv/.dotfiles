7aedb9c4f62d9fbac80b7efa9923f2e7feab526d
evf <evf@if-ufrgs>
2023-08-03 18:43:51 -0300

    update

--- evf@pop-os/.config/doom/config.el
+++ evf@pop-os/.config/doom/config.el
@@ -87,6 +87,35 @@
 
 (global-subword-mode t)                           ; Recognize uppercase letters as word boundaries (useful for CamelCase naming)
 
+;; ;; Dired
+;; (evil-define-key 'normal dired-mode-map
+;;   (kbd "M-RET") 'dired-display-file
+;;   (kbd "h") 'dired-up-directory
+;;   (kbd "l") 'dired-open-file ; use dired-find-file instead of dired-open.
+;;   (kbd "m") 'dired-mark
+;;   (kbd "t") 'dired-toggle-marks
+;;   (kbd "u") 'dired-unmark
+;;   (kbd "C") 'dired-do-copy
+;;   (kbd "D") 'dired-do-delete
+;;   (kbd "J") 'dired-goto-file
+;;   (kbd "M") 'dired-do-chmod
+;;   (kbd "O") 'dired-do-chown
+;;   (kbd "P") 'dired-do-print
+;;   (kbd "R") 'dired-do-rename
+;;   (kbd "T") 'dired-do-touch
+;;   (kbd "Y") 'dired-copy-filenamecopy-filename-as-kill ; copies filename to kill ring.
+;;   (kbd "Z") 'dired-do-compress
+;;   (kbd "+") 'dired-create-directory
+;;   (kbd "-") 'dired-do-kill-lines
+;;   (kbd "% l") 'dired-downcase
+;;   (kbd "% m") 'dired-mark-files-regexp
+;;   (kbd "% u") 'dired-upcase
+;;   (kbd "* %") 'dired-mark-files-regexp
+;;   (kbd "* .") 'dired-mark-extension
+;;   (kbd "* /") 'dired-mark-directories
+;;   (kbd "; d") 'epa-dired-do-decrypt
+;;   (kbd "; e") 'epa-dired-do-encrypt)
+
 ;; Make popups open on the right
 (set-popup-rules!
   '(("^ \\*" :slot -1)
