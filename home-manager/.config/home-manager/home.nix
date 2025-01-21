3a3df2932a08b96769e3c78f9d868dd1375b736d
evf <eliseuv816@gmail.com>
2025-01-21 17:06:13 -0300

    update

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -667,8 +667,10 @@
     settings = {
       "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
       logo = {
+        # type = "small";
         padding = {
           top = 2;
+          left = 2;
         };
       };
       display = {
