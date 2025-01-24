624674599e975a7f2302d76dda089574fef249b4
evf <eliseuv816@gmail.com>
2025-01-24 18:17:56 -0300

    Hyprland lock screen

--- desktops/hyprland/flake.nix
+++ desktops/hyprland/flake.nix
@@ -10,17 +10,30 @@
       kitty
     ];
 
+  # Catppuccin theme
+  home.file.".config/hypr/catpuccin" = {
+    source = ./hypr/catppuccin;
+    recursive = true;
+  };
+
+  # Status bar
+  programs.waybar = {
+    enable = true;
+    systemd.enable = true;
+  };
+
+  # Wallpapers
   services.hyprpaper = { enable = true; };
-  # Copy Wallpapers
   home.file."wallpapers" = {
     source = ../wallpapers;
     recursive = true;
   };
 
-  programs.waybar = {
-    enable = true;
-    systemd.enable = true;
-  };
+  # Lock screen
+  services.hypridle = { enable = true; };
+  home.file.".config/hypr/hypridle.conf".source = ./hypr/hypridle.conf;
+  programs.hyprlock = { enable = true; };
+  home.file.".config/hypr/hyprlock.conf".source = ./hypr/hyprlock.conf;
 
   wayland.windowManager.hyprland = { enable = false; };
 
