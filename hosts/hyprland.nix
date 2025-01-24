cb9de7fa49f9a3c72434e062b37cab0685e6c291
evf <eliseuv816@gmail.com>
2025-01-23 21:51:28 -0300

    update

--- hosts/hyprland.nix
+++ hosts/hyprland.nix
@@ -1,14 +1,17 @@
 { ... }: {
 
+  # Display Manager
   services.xserver = {
     enable = true;
     displayManager.sddm = {
       enable = true;
       wayland.enable = true;
       autoNumlock = true;
+      settings = { general.numlock = true; };
     };
   };
 
+  # Hyprland
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
