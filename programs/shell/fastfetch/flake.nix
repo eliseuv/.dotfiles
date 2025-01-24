2b306d9ed21c28f2ec2d521fbd53b47d973e9fb4
evf <eliseuv816@gmail.com>
2025-01-23 23:40:46 -0300

    update

--- programs/shell/fastfetch/flake.nix
+++ programs/shell/fastfetch/flake.nix
@@ -1,101 +1,99 @@
 { config, pkgs, ... }: {
 
+  home.file.".config/fastfetch/nixos.png".source = ./nixos.png;
   programs.fastfetch = {
     enable = true;
     settings = {
-      "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
+      "$schema" =
+        "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
       logo = {
-        # type = "small";
-        padding = {
-          top = 2;
-          left = 2;
-        };
+        type = "kitty";
+        source = "~/.config/fastfetch/nixos.png";
+        padding = { left = 2; };
+        height = 16;
       };
       display = {
         color = {
           keys = "green";
           title = "blue";
         };
-        percent = {
-          type = 9;
-        };
+        percent = { type = 9; };
         separator = " 󰁔 ";
       };
-      modules =
-        [
-          {
-            type = "title";
-            key = " ╭─  ";
-            keyColor = "cyan";
-            color = {
-              user = "magenta";
-              host = "blue";
-            };
-          }
-          {
-            type = "os";
-            key = " ├─  ";
-            keyColor = "cyan";
-          }
-          {
-            type = "kernel";
-            key = " ├─  ";
-            keyColor = "cyan";
-          }
-          {
-            type = "packages";
-            key = " ├─  ";
-            keyColor = "cyan";
-          }
-          {
-            type = "wm";
-            key = " ├─  ";
-            keyColor = "cyan";
-          }
-          {
-            type = "terminal";
-            key = " ├─  ";
-            keyColor = "cyan";
-          }
-          {
-            type = "shell";
-            key = " ╰─  ";
-            keyColor = "cyan";
-          }
-          {
-            type = "host";
-            key = " ╭─  ";
-            keyColor = "blue";
-          }
-          {
-            type = "cpu";
-            key = " ├─ 󰍛 ";
-            keyColor = "blue";
-          }
-          {
-            type = "memory";
-            key = " ├─  ";
-            keyColor = "blue";
-          }
-          {
-            type = "disk";
-            key = " ├─ 󱛟 ";
-            keyColor = "blue";
-            format = "{name}: {size-total} ({size-percentage})";
-          }
-          {
-            type = "gpu";
-            key = " ├─  ";
-            keyColor = "blue";
-          }
-          {
-            type = "display";
-            key = " ╰─ 󰍹 ";
-            keyColor = "blue";
-            compactType = "original-with-refresh-rate";
-          }
-          "break"
-        ];
+      modules = [
+        {
+          type = "title";
+          color = {
+            user = "magenta";
+            host = "blue";
+          };
+        }
+        "break"
+        {
+          type = "os";
+          key = " ╭─  ";
+          keyColor = "cyan";
+        }
+        {
+          type = "kernel";
+          key = " ├─  ";
+          keyColor = "cyan";
+        }
+        {
+          type = "packages";
+          key = " ├─  ";
+          keyColor = "cyan";
+        }
+        {
+          type = "wm";
+          key = " ├─  ";
+          keyColor = "cyan";
+        }
+        {
+          type = "terminal";
+          key = " ├─  ";
+          keyColor = "cyan";
+        }
+        {
+          type = "shell";
+          key = " ╰─  ";
+          keyColor = "cyan";
+        }
+        "break"
+        {
+          type = "host";
+          key = " ╭─  ";
+          keyColor = "blue";
+        }
+        {
+          type = "cpu";
+          key = " ├─ 󰍛 ";
+          keyColor = "blue";
+        }
+        {
+          type = "memory";
+          key = " ├─  ";
+          keyColor = "blue";
+        }
+        {
+          type = "disk";
+          key = " ├─ 󱛟 ";
+          keyColor = "blue";
+          format = "{name}: {size-total} ({size-percentage})";
+        }
+        {
+          type = "gpu";
+          key = " ├─  ";
+          keyColor = "blue";
+        }
+        {
+          type = "display";
+          key = " ╰─ 󰍹 ";
+          keyColor = "blue";
+          compactType = "original-with-refresh-rate";
+        }
+        "break"
+      ];
     };
   };
 
