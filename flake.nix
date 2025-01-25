073b6659445742c8ce9d2d543d742848a19efbf6
evf <eliseuv816@gmail.com>
2025-01-25 19:10:11 -0300

    Spicetify

--- flake.nix
+++ flake.nix
@@ -1,11 +1,18 @@
 {
   description = "evf's dotfiles";
   inputs = {
+    # NixOS unstable
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    # Home manager
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # Spicetify
+    spicetify-nix = {
+      url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, spicetify-nix, ... }@inputs:
     let
       system = "x86_64-linux";
       lib = nixpkgs.lib;
@@ -14,16 +21,19 @@
       nixosConfigurations = {
         GLaDOS = lib.nixosSystem {
           inherit system;
+          specialArgs = { inherit inputs; };
           modules = [ ./system/hosts/GLaDOS/configuration.nix ];
         };
         tardis = lib.nixosSystem {
           inherit system;
+          specialArgs = { inherit inputs; };
           modules = [ ./system/hosts/tardis/configuration.nix ];
         };
       };
       homeConfigurations = {
         evf = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
+          extraSpecialArgs = { inherit inputs; };
           modules = [ ./home/flake.nix ];
         };
       };
--- home/programs/media/spotify/flake.nix
+++ home/programs/media/spotify/flake.nix
@@ -1,5 +1,18 @@
-{ config, pkgs, ... }: {
+{ pkgs, inputs, ... }: {
 
-  home.packages = with pkgs; [ spotify spicetify-cli ];
+  imports = [ inputs.spicetify-nix.homeManagerModules.default ];
+
+  programs.spicetify =
+    let spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+    in {
+      enable = true;
+      enabledExtensions = with spicePkgs.extensions; [
+        adblock
+        hidePodcasts
+        shuffle # shuffle+ (special characters are sanitized out of extension names)
+      ];
+      theme = spicePkgs.themes.catppuccin;
+      colorScheme = "mocha";
+    };
 
 }
