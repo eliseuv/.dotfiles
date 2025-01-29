67c8d2592d6dcbd7bb2c2e8d69bc2f7e6d14e388
evf <eliseuv816@gmail.com>
2025-01-29 10:45:19 -0300

    65 current  2025-01-29 10:45:16  25.05.20250126.852ff1d  6.6.74                          *

--- system/hardware/network.nix
+++ system/hardware/network.nix
@@ -7,7 +7,12 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Avahi
-  services.avahi = { enable = true; };
+  services.avahi = {
+    enable = true;
+
+    # mDNS NSS plug-in for IPv4 allows applications to resolve names in the .local domain by transparently querying the Avahi daemon
+    nssmdns4 = true;
+  };
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
