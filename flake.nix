1f7574ffd6eb79fcf489d3db6eaef231e65b7e7d
evf <eliseuv816@gmail.com>
2025-01-29 18:12:03 -0300

    66 current  2025-01-29 18:10:39  25.05.20250126.852ff1d  6.6.74                          *

--- flake.nix
+++ flake.nix
@@ -6,13 +6,17 @@
     # Home manager
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # Nix Index Database
+    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
     # Spicetify
     spicetify-nix = {
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, home-manager, spicetify-nix, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, nix-index-database, spicetify-nix
+    , ... }@inputs:
     let
       system = "x86_64-linux";
       lib = nixpkgs.lib;
@@ -22,7 +26,13 @@
         GLaDOS = lib.nixosSystem {
           inherit system;
           specialArgs = { inherit inputs; };
-          modules = [ ./system/hosts/GLaDOS/configuration.nix ];
+          modules = [
+            # Host configuration
+            ./system/hosts/GLaDOS/configuration.nix
+            # Nix index database
+            nix-index-database.nixosModules.nix-index
+            { programs.nix-index-database.comma.enable = true; }
+          ];
         };
         tardis = lib.nixosSystem {
           inherit system;
