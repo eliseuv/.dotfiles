ab36b079300225dba2aef45918ae75fe112271f8
evf <eliseuv816@gmail.com>
2025-01-31 22:50:12 -0300

    flake tweaks and home-manager filename

--- flake.nix
+++ flake.nix
@@ -1,22 +1,30 @@
 {
+
   description = "evf's dotfiles";
+
   inputs = {
+
     # NixOS unstable
     nixpkgs.url = "nixpkgs/nixos-unstable";
+
     # Home Manager
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     # Nix Index Database
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+
     # Spicetify
     spicetify-nix = {
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
   };
+
   outputs = { self, nixpkgs, home-manager, nix-index-database, spicetify-nix
     , ... }@inputs:
     let
@@ -24,31 +32,38 @@
       lib = nixpkgs.lib;
       pkgs = nixpkgs.legacyPackages.${system};
     in {
+
       nixosConfigurations = {
+
         GLaDOS = lib.nixosSystem {
           inherit system;
           specialArgs = { inherit inputs; };
           modules = [ ./system/hosts/GLaDOS/configuration.nix ];
         };
+
         tardis = lib.nixosSystem {
           inherit system;
           specialArgs = { inherit inputs; };
           modules = [ ./system/hosts/tardis/configuration.nix ];
         };
+
       };
-      homeConfigurations = {
-        evf = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          extraSpecialArgs = { inherit inputs; };
-          modules = [
-            ./home/flake.nix
-            # Nix Index Database
-            nix-index-database.hmModules.nix-index
-            # optional to also wrap and install comma
-            { programs.nix-index-database.comma.enable = true; }
-          ];
-        };
+
+      homeConfigurations.evf = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = { inherit inputs; };
+        modules = [
+          ./home/home.nix
+
+          # Nix Index Database
+          nix-index-database.hmModules.nix-index
+
+          # optional to also wrap and install comma
+          { programs.nix-index-database.comma.enable = true; }
+        ];
       };
+
     };
+
 }
 
