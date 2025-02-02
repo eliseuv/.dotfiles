c0abe47bc3d10ebb6e8f66969df39f0c9d62a9d9
evf <eliseuv816@gmail.com>
2025-02-02 00:17:29 -0300

    emacs dependencies to compile vterm attempt

--- home/programs/editors/emacs/flake.nix
+++ home/programs/editors/emacs/flake.nix
@@ -1,6 +1,14 @@
 { pkgs, ... }: {
 
-  home.packages = with pkgs; [ alegreya ibm-plex symbola ];
+  home.packages = with pkgs; [
+    cmakeMinimal
+    # Fonts
+    alegreya
+    ibm-plex
+    symbola
+  ];
+
+  home.sessionVariables = { CMAKE_C_COMPILER = "${pkgs.gcc}/bin/gcc"; };
 
   home.file = {
     ".config/doom" = {
