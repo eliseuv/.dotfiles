b3d311e9cffbce88d6388be4d9d0b1491f146aac
evf <eliseuv816@gmail.com>
2025-02-06 15:11:20 -0300

    Julia REPL startup script and nvim-lspconfig

--- home/shell/dev/julia.nix
+++ home/shell/dev/julia.nix
@@ -1,5 +1,14 @@
 { pkgs, ... }: {
 
-  home.packages = with pkgs; [ julia ];
+  home.packages = with pkgs; [ julia-bin ];
+
+  home.file = {
+    # REPL startup script
+    ".julia/config/startup.jl".source = ./julia/startup.jl;
+    # LSP requirements makefile
+    ".julia/environments/nvim-lspconfig/Makefile".source = ./julia/Makefile;
+    # Pluto notebook templates
+    ".julia/pluto_notebooks/ingredients.jl".source = ./julia/ingredients.jl;
+  };
 
 }
