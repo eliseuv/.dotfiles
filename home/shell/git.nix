f3531e7e5ea7516942c1d5fd959dec162ec4297c
evf <eliseuv816@gmail.com>
2025-02-08 11:09:20 -0300

    git delta config

--- home/shell/git.nix
+++ home/shell/git.nix
@@ -4,7 +4,27 @@
     enable = true;
     userName = "evf";
     userEmail = "eliseuv816@gmail.com";
-    delta.enable = true;
+    delta = {
+      enable = true;
+      options = {
+        features = "decorations";
+        navigate = true; # use n and N to move between diff sections
+        light =
+          false; # set to true if you're in a terminal w/ a light background color (e.g. the default macOS terminal)
+        line-numbers = true;
+        side-by-side = false;
+        interactive = { keep-plus-minus-markers = false; };
+        decorations = {
+          commit-decoration-style = "blue ol";
+          commit-style = "raw";
+          file-style = "omit";
+          hunk-header-decoration-style = "blue box";
+          hunk-header-file-style = "red";
+          hunk-header-line-number-style = "#067a00";
+          hunk-header-style = "file line-number syntax";
+        };
+      };
+    };
   };
   home.shellAliases = {
     git-push = ''git add .; git commit -m "update"; git push'';
