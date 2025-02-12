7c10dfc6be81b77850627f807ff5a97376353454
evf <eliseuv816@gmail.com>
2025-02-11 23:09:40 -0300

    tmux config organization

--- home/shell/multiplexer/tmux.nix
+++ home/shell/multiplexer/tmux.nix
@@ -3,17 +3,40 @@
   programs.tmux = {
     enable = true;
     keyMode = "vi";
+    terminal = "tmux-256color";
     baseIndex = 1;
     clock24 = true;
     historyLimit = 10000;
     newSession = true;
     sensibleOnTop = true;
     plugins = with pkgs; [
-      tmuxPlugins.vim-tmux-navigator
       tmuxPlugins.yank
-      tmuxPlugins.tmux-floax
       tmuxPlugins.tmux-thumbs
-      tmuxPlugins.catppuccin
+      tmuxPlugins.vim-tmux-navigator
+      {
+        plugin = tmuxPlugins.tmux-floax;
+        extraConfig = ''
+          set -g @floax-bind 'C-b'
+          set -g @floax-width '90%'
+          set -g @floax-height '90%'
+        '';
+      }
+      {
+        plugin = tmuxPlugins.catppuccin;
+        extraConfig = ''
+          set -g status-position top
+          set -g status-right-length 100
+          set -g status-left-length 100
+          set -g status-left ""
+          set -g status-right "#{E:@catppuccin_status_application}"
+          set -agF status-right "#{E:@catppuccin_status_cpu}"
+          set -ag status-right "#{E:@catppuccin_status_session}"
+          set -ag status-right "#{E:@catppuccin_status_uptime}"
+          set -agF status-right "#{E:@catppuccin_status_battery}"
+          set -g @catppuccin_flavor "mocha"
+          set -g @catppuccin_window_status_style "rounded"
+        '';
+      }
       tmuxPlugins.cpu
       tmuxPlugins.battery
     ];
@@ -29,21 +52,6 @@
       # Ctrl-Shift vim keys to switch windows
       bind -n C-S-h previous-window
       bind -n C-S-l next-window
-
-      # Floax
-      set -g @floax-bind 'C-b'
-      set -g @floax-width '90%'
-      set -g @floax-height '90%'
-
-      # Status bar
-      set -g status-position top
-
-      # Catppuccin
-      set -g @catppuccin_flavor "mocha"
-      set -g @catppuccin_window_status_style "rounded"
-      set -g status-right "#{E:@catppuccin_status_application}"
-      set -agF status-right "#{E:@catppuccin_status_cpu}"
-      set -agF status-right "#{E:@catppuccin_status_battery}"
     '';
   };
 
