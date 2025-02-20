2575cbc3f887a3bb4a5b855789247eb365e11682
evf <eliseuv816@gmail.com>
2025-02-19 21:45:48 -0300

    removed some old config files

--- .old/screenlayouts/if-ufrgs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI-1 --primary --mode 1920x1080 --pos 1280x0 --rotate normal --output DP-1 --off --output HDMI-2 --off --output DP-2 --off --output HDMI-3 --off --output DP-3 --mode 1280x1024 --pos 0x0 --rotate normal
