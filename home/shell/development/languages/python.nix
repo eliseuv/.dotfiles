4582a096905a545c66daf9b1b0b8e3238b9d85d0
evf <eliseuv816@gmail.com>
2025-02-20 11:08:28 -0300

    ruff home-manager module

--- home/shell/development/languages/python.nix
+++ home/shell/development/languages/python.nix
@@ -1,11 +1,14 @@
 { pkgs, ... }: {
 
   home.packages = with pkgs; [
+
     python3
+
     # Package and project manager
     uv
-    # Linter
-    ruff
+
   ];
+
+  programs.ruff = { enable = true; };
 
 }
