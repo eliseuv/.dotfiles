310d0971a3e63406a0d84820fa15997045ec40ff
evf <eliseuv816@gmail.com>
2025-02-20 16:25:11 -0300

    nautilus default file manager for udiskie

--- home/services/automount.nix
+++ home/services/automount.nix
@@ -7,7 +7,7 @@
     settings = {
       program_options = {
         # https://github.com/nix-community/home-manager/issues/632#issuecomment-2210425312
-        file_manager = "${pkgs.ghostty}/bin/ghostty -e ${pkgs.yazi}/bin/yazi";
+        file_manager = "${pkgs.nautilus}/bin/nautilus";
       };
     };
   };
