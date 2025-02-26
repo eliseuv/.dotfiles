594a5bb08375011987849fc3ff35bd4d2965b13e
evf <eliseuv816@gmail.com>
2025-02-25 21:14:45 -0300

    sioyek config

--- home/programs/documents/sioyek.nix
+++ home/programs/documents/sioyek.nix
@@ -1,5 +1,19 @@
 { ... }: {
 
-  programs.sioyek = { enable = true; };
+  programs.sioyek = {
+    enable = true;
+    config = {
+      # Use dark mode by default
+      "default_dark_mode" = "1";
+    };
+    bindings = {
+      # Scroll screen
+      "screen_up" = "<S-k>";
+      "screen_down" = "<S-j>";
+      # Scroll pages
+      "previous_page" = "<C-p>";
+      "next_page" = "<C-n>";
+    };
+  };
 
 }
