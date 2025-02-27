acfe8b252beab5d044e574032ce68d80d4294016
evf <eliseuv816@gmail.com>
2025-02-27 10:27:27 -0300

    doom emacs update and gc systemd timers

--- home/programs/editor/emacs.nix
+++ home/programs/editor/emacs.nix
@@ -30,4 +30,28 @@
 
   home.sessionPath = [ "$HOME/.config/emacs/bin" ];
 
+  # Doom Emacs update
+  systemd.user.services.doom-update = {
+    Unit.Description = "Update Doom Emacs";
+    Service = {
+      Type = "oneshot";
+      ExecStart = "/home/evf/.config/emacs/bin/doom sync -u --aot --force";
+      Environment =
+        "PATH=/run/current-system/sw/bin::/home/evf/.nix-profile/bin";
+    };
+    Install.WantedBy = [ "default.target" ];
+  };
+
+  # Doom Emacs gc
+  systemd.user.services.doom-gc = {
+    Unit.Description = "Garbage collect Doom Emacs";
+    Service = {
+      Type = "oneshot";
+      ExecStart = "/home/evf/.config/emacs/bin/doom gc --force";
+      Environment =
+        "PATH=/run/current-system/sw/bin::/home/evf/.nix-profile/bin";
+    };
+    Install.WantedBy = [ "default.target" ];
+  };
+
 }
