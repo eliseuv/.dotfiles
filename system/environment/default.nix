9e395f6f11561e25f74fca0c80c880f7d375d866
evf <eliseuv816@gmail.com>
2025-03-03 01:11:42 -0300

    Revert "remove duplicate allowUnfree setting"
    This reverts commit 726d0c718474b1ce7f6e076b111fea2daaf81c10.

--- system/environment/default.nix
+++ system/environment/default.nix
@@ -3,6 +3,8 @@
   imports =
     [ ./users.nix ./development.nix ./locale.nix ./time.nix ./gnupg.nix ];
 
+  nixpkgs.config.allowUnfree = true;
+
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
 
