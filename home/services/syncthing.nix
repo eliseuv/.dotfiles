0cc81d87268390987e20016a4d5f6390a4944f7a
evf <eliseuv816@gmail.com>
2025-03-04 10:47:02 -0300

    remove redundant enable in syncthing folder settings

--- home/services/syncthing.nix
+++ home/services/syncthing.nix
@@ -4,24 +4,28 @@
     enable = true;
     tray.enable = true;
     settings = {
+
       devices = {
+
         A53.id =
           "7IIH4V5-BZAZC7A-JX46YON-TIKGKIJ-X7MFSPQ-C4Q5MMS-RNJAMAY-MSRZDQM";
+
         GLaDOS.id =
           "UX6PZ74-S4VYOHG-ZWPX2ME-2ONFFNN-GWWRYCJ-4S7ZIGW-P3Z3ITG-WWELFAV";
+
         TARDIS.id =
           "NR7XLUG-MNAFRJS-IKP3EGN-TBHXJMW-YGMDSES-MLPOKYD-I5PJ23K-J7AJZQE";
+
       };
+
       folders = {
 
         "music" = {
-          enable = true;
           path = "/run/media/evf/Storage/CompanionCube/music";
           devices = [ "A53" ];
         };
 
         "org" = {
-          enable = true;
           path = "~/Documents/org";
           devices = [ "GLaDOS" "TARDIS" ];
         };
