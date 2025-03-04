e75d140f151f9e9af0f8f88004391925d4930b61
evf <eliseuv816@gmail.com>
2025-03-03 23:52:33 -0300

    add GLaDOS to syncthing

--- home/services/syncthing.nix
+++ home/services/syncthing.nix
@@ -6,6 +6,8 @@
       devices = {
         galaxy-A53.id =
           "7IIH4V5-BZAZC7A-JX46YON-TIKGKIJ-X7MFSPQ-C4Q5MMS-RNJAMAY-MSRZDQM";
+        GLaDOS.id =
+          "UX6PZ74-S4VYOHG-ZWPX2ME-2ONFFNN-GWWRYCJ-4S7ZIGW-P3Z3ITG-WWELFAV";
       };
       folders = {
         "music" = {
