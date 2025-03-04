0ada04c6fac8585050bd3e37bd464a10dbbadca2
evf <eliseuv816@gmail.com>
2025-03-04 10:32:28 -0300

    rename phone syncthing device name

--- home/services/syncthing.nix
+++ home/services/syncthing.nix
@@ -4,7 +4,7 @@
     enable = true;
     settings = {
       devices = {
-        galaxy-A53.id =
+        A53.id =
           "7IIH4V5-BZAZC7A-JX46YON-TIKGKIJ-X7MFSPQ-C4Q5MMS-RNJAMAY-MSRZDQM";
         GLaDOS.id =
           "UX6PZ74-S4VYOHG-ZWPX2ME-2ONFFNN-GWWRYCJ-4S7ZIGW-P3Z3ITG-WWELFAV";
@@ -16,7 +16,7 @@
         "music" = {
           enable = true;
           path = "/run/media/evf/Storage/CompanionCube/music";
-          devices = [ "galaxy-A53" ];
+          devices = [ "A53" ];
         };
 
         "org" = {
