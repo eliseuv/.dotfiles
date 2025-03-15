d5b6c0bf3198bfb7532d4b56cd2994004dce5b03
evf <eliseuv816@gmail.com>
2025-03-15 18:32:16 -0300

    disable sshd password authentication

--- system/hardware/network.nix
+++ system/hardware/network.nix
@@ -15,7 +15,10 @@
   };
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings = { PasswordAuthentication = false; };
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
