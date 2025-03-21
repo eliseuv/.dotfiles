6dfd6ff488d47904a4754d6e9ee78ac11d3342aa
evf <eliseuv816@gmail.com>
2025-03-21 20:51:45 -0300

    disable doom emacs update and gc timers

--- home/services/timers.nix
+++ home/services/timers.nix
@@ -14,19 +14,19 @@
       Install.WantedBy = [ "timers.target" ];
     };
 
-    # Doom Emacs update
-    doom-update = {
-      Unit.Description = "Scheduled Doom Emacs update";
-      Timer = { OnCalendar = "daily"; };
-      Install.WantedBy = [ "timers.target" ];
-    };
+    # # Doom Emacs update
+    # doom-update = {
+    #   Unit.Description = "Scheduled Doom Emacs update";
+    #   Timer = { OnCalendar = "daily"; };
+    #   Install.WantedBy = [ "timers.target" ];
+    # };
 
-    # Doom Emacs garbage collection
-    doom-gc = {
-      Unit.Description = "Scheduled Doom Emacs garbage collection";
-      Timer = { OnCalendar = "weekly"; };
-      Install.WantedBy = [ "timers.target" ];
-    };
+    # # Doom Emacs garbage collection
+    # doom-gc = {
+    #   Unit.Description = "Scheduled Doom Emacs garbage collection";
+    #   Timer = { OnCalendar = "weekly"; };
+    #   Install.WantedBy = [ "timers.target" ];
+    # };
 
   };
 
