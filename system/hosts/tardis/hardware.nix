dd22df18f33c79703bdb68b5b519b6480f4af4af
evf <eliseuv816@gmail.com>
2025-04-03 20:00:24 -0300

    move enable numlock on early boot to configuration.nix

--- system/hosts/tardis/hardware.nix
+++ system/hosts/tardis/hardware.nix
@@ -12,17 +12,6 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  # Enable num lock early on boot
-  boot.initrd.extraUtilsCommands = ''
-    copy_bin_and_libs ${pkgs.kbd}/bin/setleds
-  '';
-  boot.initrd.preDeviceCommands = ''
-    INITTY=/dev/tty[1-6]
-    for tty in $INITTY; do
-      /bin/setleds -D +num < $tty
-    done
-  '';
-
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/9c2cbe1d-9a12-4784-b903-6822cbe6fd34";
     fsType = "ext4";
