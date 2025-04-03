77263889f68a13f04e1b2fae422f83e7540397e5
evf <eliseuv816@gmail.com>
2025-04-03 20:01:26 -0300

    systemd service to enable numlock on startup

--- system/hardware/keyboard.nix
+++ system/hardware/keyboard.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, lib, ... }: {
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -36,6 +36,19 @@
           )
         '';
       };
+    };
+  };
+
+  # Enable num lock early on boot
+  systemd.services.numLockOnTty = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      # /run/current-system/sw/bin/setleds -D +num < "$tty";
+      ExecStart = lib.mkForce (pkgs.writeShellScript "numLockOnTty" ''
+        for tty in /dev/tty{1..6}; do
+            ${pkgs.kbd}/bin/setleds -D +num < "$tty";
+        done
+      '');
     };
   };
 
