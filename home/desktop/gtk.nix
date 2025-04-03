32a7cd1d683d101b071b96026b44f300e4d5c52e
evf <eliseuv816@gmail.com>
2025-04-03 17:42:33 -0300

    gdm numlock key on by default

--- home/desktop/gtk.nix
+++ home/desktop/gtk.nix
@@ -2,6 +2,10 @@
 
   dconf.settings = {
     "org/gnome/desktop/interface" = { color-scheme = "prefer-dark"; };
+    "org/gnome/desktop/peripherals/keyboard" = {
+      numlock-state = true;
+      remember-numlock-state = true;
+    };
   };
 
   gtk = {
