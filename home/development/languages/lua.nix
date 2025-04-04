c595d792f64b124d58d35a424fbccc7273f3f8f5
evf <eliseuv816@gmail.com>
2025-04-04 11:40:36 -0300

    install lua-ls

--- home/development/languages/lua.nix
+++ home/development/languages/lua.nix
@@ -1,4 +1,10 @@
 { pkgs, ... }: {
 
-  home.packages = with pkgs; [ lua5_1 lua51Packages.luarocks ];
+  home.packages = with pkgs; [
+    lua5_1
+    # LSP
+    lua-language-server
+    # Lua Rocks
+    lua51Packages.luarocks
+  ];
 }
