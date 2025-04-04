bb60ba942861d0d6e531f029e9b2d8525cb8d9bb
evf <eliseuv816@gmail.com>
2025-04-04 11:46:03 -0300

    install additional lua development packages

--- home/development/languages/lua.nix
+++ home/development/languages/lua.nix
@@ -1,10 +1,16 @@
 { pkgs, ... }: {
 
   home.packages = with pkgs; [
+    # Lua version pinned because of neovim
     lua5_1
     # LSP
     lua-language-server
+    # Formatter
+    stylua
+    # Linter
+    ast-grep
     # Lua Rocks
     lua51Packages.luarocks
   ];
+
 }
