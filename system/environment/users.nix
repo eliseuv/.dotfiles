0e4096060c8cbea0640227d720947731b644a3ea
evf <eliseuv816@gmail.com>
2025-04-09 09:23:20 -0300

    [nixos] enable linger for evf

--- system/environment/users.nix
+++ system/environment/users.nix
@@ -1,16 +1,25 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
 
   users.users = {
 
     evf = {
       isNormalUser = true;
       description = "evf";
-      extraGroups = [ "wheel" "networkmanager" "libvirtd" ];
+      extraGroups = [
+        "wheel"
+        "networkmanager"
+        "libvirtd"
+      ];
+      linger = true;
       packages = with pkgs; [ ];
     };
 
   };
 
-  nix.settings.trusted-users = [ "root" "@wheel" ];
+  nix.settings.trusted-users = [
+    "root"
+    "@wheel"
+  ];
 
 }
