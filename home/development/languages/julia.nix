087cdcc7c44772da9662f2a86f173bd0e376e7e0
evf <eliseuv816@gmail.com>
2025-04-23 18:59:14 -0300

    [julia] schedule daily environment update

--- home/development/languages/julia.nix
+++ home/development/languages/julia.nix
@@ -1,4 +1,5 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }:
+{
 
   home.packages = with pkgs; [ julia-bin ];
 
@@ -11,12 +12,43 @@
     ".julia/pluto_notebooks/ingredients.jl".source = ./julia/ingredients.jl;
   };
 
-  home.sessionVariables = { JULIA_NUM_THREADS = "auto"; };
+  home.sessionVariables = {
+    JULIA_NUM_THREADS = "auto";
+  };
 
   home.shellAliases = {
     julia-env-update = "julia --eval 'using Pkg; Pkg.update()'";
     julia-env-gc = "julia --eval 'using Pkg; Pkg.gc()'";
     pluto-run = "julia --eval 'using Pluto; Pluto.run(launch_browser=false)'";
+  };
+
+  # Julia environment update
+  systemd.user.services.julia-update = {
+    Unit = {
+      Description = "Update Julia environment";
+      Wants = [
+        "network.target"
+        "nss-lookup.target"
+      ];
+      After = [
+        "network.target"
+        "nss-lookup.target"
+      ];
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = ''${lib.getExe pkgs.julia-bin} --eval "using Pkg; Pkg.update()"'';
+    };
+    Install.WantedBy = [ "default.target" ];
+  };
+
+  systemd.user.timers.julia-update = {
+    Unit.Description = "Scheduled Julia environment update";
+    Timer = {
+      OnCalendar = "daily";
+      Persistent = true;
+    };
+    Install.WantedBy = [ "timers.target" ];
   };
 
 }
