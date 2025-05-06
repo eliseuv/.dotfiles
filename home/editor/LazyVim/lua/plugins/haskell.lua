c0e36899219f985dae13b1e51e03c044df6370f0
evf <eliseuv816@gmail.com>
2025-05-05 22:54:30 -0300

    [neovim] remove custom haskell lazy.nvim plugin

--- home/editor/LazyVim/lua/plugins/haskell.lua
+++ home/editor/LazyVim/lua/plugins/haskell.lua
@@ -1,8 +1,1 @@
-return {
-    -- Haskell tools
-    {
-        "mrcjkb/haskell-tools.nvim",
-        version = "^4", -- Recommended
-        lazy = false, -- This plugin is already lazy
-    },
-}
+return {}
