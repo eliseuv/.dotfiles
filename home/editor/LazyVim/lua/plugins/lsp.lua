aa3d8e64cddcf992631b717b6d382eafa9b3a7f0
evf <eliseuv816@gmail.com>
2025-05-06 21:37:52 -0300

    [neovim] pin mason version
    Mason broken on LazyVim (https://github.com/LazyVim/LazyVim/issues/6039)

--- home/editor/LazyVim/lua/plugins/lsp.lua
+++ home/editor/LazyVim/lua/plugins/lsp.lua
@@ -2,10 +2,17 @@ return {
 
   {
     "williamboman/mason.nvim",
+    -- Pin version
+    version = "1.11.0",
     -- enabled = false,
     opts = {
       -- Prefer local binaries over mason binaries
       PATH = "append",
     },
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    -- Pin version
+    version = "1.32.0",
   },
 }
