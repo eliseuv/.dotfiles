dabe5cf3780c0bb52dfd7ec198daa1aaaa961c2d
evf <eliseuv816@gmail.com>
2025-05-29 20:59:28 -0300

    [vm] use QEMU KVM and enable OVMF, TPM emulation and NSS

--- system/extra/virtual-machines.nix
+++ system/extra/virtual-machines.nix
@@ -2,16 +2,28 @@
 {
 
   virtualisation = {
-    libvirtd.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu = {
+        # QEMU KVM emulates only host arch
+        package = pkgs.qemu_kvm;
+        # OVMF
+        ovmf = {
+          enable = true;
+          packages = [ pkgs.OVMFFull.fd ];
+        };
+        # TPM Emulation
+        swtpm.enable = true;
+      };
+      # NSS
+      nss.enable = true;
+    };
     spiceUSBRedirection.enable = true;
   };
 
   programs.virt-manager.enable = true;
 
   environment.systemPackages = with pkgs; [
-
-    # TPM Emulation
-    swtpm
 
     # Templates
     quickemu
