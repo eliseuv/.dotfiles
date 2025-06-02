976d2c24f57cab335f77d0e64467d02053cd4827
evf <eliseuv816@gmail.com>
2025-06-02 16:26:50 -0300

    [langs] unpin Lua version

--- home/development/languages/lua.nix
+++ home/development/languages/lua.nix
@@ -2,14 +2,17 @@
 {
 
   home.packages = with pkgs; [
-    # Lua version pinned because of neovim
-    lua5_1
+
+    lua
+
     # LSP
     lua-language-server
+
     # Formatter
     stylua
+
     # Lua Rocks
-    lua51Packages.luarocks
+    luarocks
   ];
 
 }
