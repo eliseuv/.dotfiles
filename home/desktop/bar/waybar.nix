e21717da26badc5dad94b26fda501b060f90a10c
evf <eliseuv816@gmail.com>
2025-06-07 10:28:21 -0300

    [waybar] modules specification and styling

--- home/desktop/bar/waybar.nix
+++ home/desktop/bar/waybar.nix
@@ -6,4 +6,10 @@
     systemd.enable = true;
   };
 
+  # Modules
+  home.file.".config/waybar/modules.json".source = ./waybar/modules.json;
+
+  # Styling
+  home.file.".config/waybar/style.css".source = ./waybar/style.css;
+
 }
