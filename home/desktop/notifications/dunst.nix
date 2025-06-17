0d00a6020992ac21449cfdce0246f405078a0fe5
evf <eliseuv816@gmail.com>
2025-06-17 15:23:04 -0300

    [dunst] configure appearance

--- home/desktop/notifications/dunst.nix
+++ home/desktop/notifications/dunst.nix
@@ -6,10 +6,30 @@
     # Provides `notify-send`
     libnotify
 
+    # Font
+    nerd-fonts.ubuntu
+
   ];
 
   services.dunst = {
     enable = true;
+    settings = {
+      global = {
+        origin = "top-center";
+        offset = "(0, 5)";
+        follow = "keyboard";
+        font = "Ubuntu Nerd Font 9";
+        width = "(0, 300)";
+        frame_width = 2;
+        corner_radius = 5;
+      };
+      urgency_normal = {
+        background = "#1e1d2f";
+        foreground = "#bd93f9";
+        frame_color = "#5e497c";
+        timeout = 10;
+      };
+    };
   };
 
 }
