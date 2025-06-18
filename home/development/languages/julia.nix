eb39f85645c36f834706cb9acdc45f16310c317a
evf <eliseuv816@gmail.com>
2025-06-18 10:57:00 -0300

    [julia] environment update and cleanup scripts

--- home/development/languages/julia.nix
+++ home/development/languages/julia.nix
@@ -1,7 +1,24 @@
 { lib, pkgs, ... }:
+let
+  # Update environment script
+  julia-env-update = pkgs.writeShellScriptBin "julia-env-update" ''
+    ${lib.getExe pkgs.julia-bin} --eval "using Pkg; Pkg.update()" && ${pkgs.libnotify}/bin/notify-send "Julia" "Environment update completed" || ${pkgs.libnotify}/bin/notify-send "Julia" "Environment update failed" -u critical
+  '';
+  # Cleanup environment script
+  julia-env-gc = pkgs.writeShellScriptBin "julia-env-gc" ''
+    ${lib.getExe pkgs.julia-bin} --eval "using Pkg; Pkg.gc()" && ${pkgs.libnotify}/bin/notify-send "Julia" "Environment cleanup completed" || ${pkgs.libnotify}/bin/notify-send "Julia" "Environment cleanup failed" -u critical
+  '';
+in
 {
 
-  home.packages = with pkgs; [ julia-bin ];
+  home.packages = with pkgs; [
+    julia-bin
+
+    # Environment management scripts
+    julia-env-update
+    julia-env-gc
+
+  ];
 
   home.file = {
     # REPL startup script
@@ -17,13 +34,11 @@
   };
 
   home.shellAliases = {
-    julia-env-update = "julia --eval 'using Pkg; Pkg.update()'";
-    julia-env-gc = "julia --eval 'using Pkg; Pkg.gc()'";
     pluto-run = "julia --eval 'using Pluto; Pluto.run(launch_browser=false)'";
   };
 
   # Julia environment update
-  systemd.user.services.julia-update = {
+  systemd.user.services.julia-env-update = {
     Unit = {
       Description = "Update Julia environment";
       Wants = [
@@ -37,13 +52,34 @@
     };
     Service = {
       Type = "oneshot";
-      ExecStart = ''${lib.getExe pkgs.julia-bin} --eval "using Pkg; Pkg.update()"'';
+      ExecStart = lib.getExe julia-env-update;
     };
     Install.WantedBy = [ "default.target" ];
   };
 
-  systemd.user.timers.julia-update = {
+  systemd.user.timers.julia-env-update = {
     Unit.Description = "Scheduled Julia environment update";
+    Timer = {
+      OnCalendar = "daily";
+      Persistent = true;
+    };
+    Install.WantedBy = [ "timers.target" ];
+  };
+
+  # Julia environment cleanup
+  systemd.user.services.julia-env-gc = {
+    Unit = {
+      Description = "Cleanup Julia environment";
+    };
+    Service = {
+      Type = "oneshot";
+      ExecStart = lib.getExe julia-env-gc;
+    };
+    Install.WantedBy = [ "default.target" ];
+  };
+
+  systemd.user.timers.julia-env-gc = {
+    Unit.Description = "Scheduled Julia environment cleanup";
     Timer = {
       OnCalendar = "daily";
       Persistent = true;
