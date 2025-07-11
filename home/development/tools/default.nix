2bb47f314cd83498eeabdeffb3d4ded721911c71
evf <eliseuv816@gmail.com>
2025-07-11 16:44:16 -0300

    [dev] move just to separate module

--- home/development/tools/default.nix
+++ home/development/tools/default.nix
@@ -1,12 +1,15 @@
 { pkgs, ... }:
 {
 
+  imports = [
+    ./just.nix
+  ];
+
   home.packages = with pkgs; [
 
     # Command runner
     gnumake
     cmake
-    just
 
     # Run command on change
     watchexec
