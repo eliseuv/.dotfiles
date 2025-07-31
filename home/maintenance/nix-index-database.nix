93ff461f4105d2314645610a332a7d7c552f822b
evf <eliseuv816@gmail.com>
2025-07-31 11:50:45 -0300

    [nix-index-db] fix deprecated name

--- home/maintenance/nix-index-database.nix
+++ home/maintenance/nix-index-database.nix
@@ -1,7 +1,7 @@
 { inputs, ... }:
 {
 
-  imports = [ inputs.nix-index-database.hmModules.nix-index ];
+  imports = [ inputs.nix-index-database.homeModules.nix-index ];
   programs.nix-index-database.comma.enable = true;
 
 }
