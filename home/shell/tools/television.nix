d6787d52a7e9b78588f4274345a0bafd60289acf
evf <eliseuv816@gmail.com>
2025-08-04 21:21:33 -0300

    [television] shell channel triggers

--- home/shell/tools/television.nix
+++ home/shell/tools/television.nix
@@ -6,6 +6,21 @@
     enableZshIntegration = true;
     channels = { };
     settings = {
+      shell_integration = {
+        channel_triggers = {
+          dirs = [
+            "cd"
+            "ls"
+            "l"
+            "ll"
+          ];
+          files = [
+            "cat"
+            "bat"
+            "b"
+          ];
+        };
+      };
       tick_rate = 50;
       ui = {
         use_nerd_font_icons = true;
