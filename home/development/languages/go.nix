c0bbcca5a7ff8509f0662644b77d7b8dccbf7146
evf <eliseuv816@gmail.com>
2025-08-19 16:47:39 -0300

    [go] install additional tools

--- home/development/languages/go.nix
+++ home/development/languages/go.nix
@@ -1,5 +1,26 @@
-{ ... }:
+{ pkgs, ... }:
 {
+
+  home.packages = with pkgs; [
+
+    # LSP
+    gopls
+
+    # godoc, goimports, etc.
+    gotools
+
+    # Formatter
+    gofumpt
+
+    # Modify struct field tags
+    gomodifytags
+    # Method stubs for interfaces
+    impl
+
+    # Debugger
+    delve
+
+  ];
 
   programs.go = {
     enable = true;
