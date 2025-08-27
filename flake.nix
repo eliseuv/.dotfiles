3811945f17928f121c7fda50ac7bd009a5313f47
evf <eliseuv816@gmail.com>
2025-08-27 12:26:51 -0300

    [flake] remove NixOS cache and nix-community cachix servers
    This reverts commit 22069446817e67962057b9937bea4b8fc5b6a4eb.
    The derivations became too slow.

--- flake.nix
+++ flake.nix
@@ -2,19 +2,6 @@
 
   description = "evf's dotfiles";
 
-  nixConfig = {
-
-    substituters = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-    ];
-
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-
-  };
-
   inputs = {
 
     # NixOS unstable
