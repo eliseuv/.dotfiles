c03c6b478c3008483750ca058915e50fcac74be1
evf <eliseuv816@gmail.com>
2025-09-19 16:19:21 -0300

    [kitty] config kitty-scrollback.nvim

--- home/terminal/kitty.nix
+++ home/terminal/kitty.nix
@@ -16,6 +16,8 @@
       scrollback_lines = 10000;
       scrollback_pager_history_size = 64;
       hide_window_decorations = "yes";
+      allow_remote_control = "socket-only";
+      listen_on = "unix:/tmp/kitty";
     };
 
     keybindings = {
@@ -24,6 +26,20 @@
       "kitty_mod+p>d" = "kitten hints --program @ --type url";
       "kitty_mod+p>c" = "kitten hints --type path --program @";
       "kitty_mod+space" = "kitten unicode_input";
+
+      # Browse scrollback buffer in nvim
+      "kitty_mod+h" = "kitty_scrollback_nvim";
+      # Browse output of the last shell command in nvim
+      "kitty_mod+g" = "kitty_scrollback_nvim --config ksb_builtin_last_cmd_output";
+      # Show clicked command output in nvim
+      # mouse_map ctrl+shift+right press ungrabbed combine : mouse_select_command_output : kitty_scrollback_nvim --config ksb_builtin_last_visited_cmd_output
+    };
+
+    actionAliases = {
+      # kitty-scrollback.nvim Kitten alias
+      "kitty_scrollback_nvim" =
+        "kitten /home/evf/.local/share/nvim/lazy/kitty-scrollback.nvim/python/kitty_scrollback_nvim.py";
+
     };
 
     shellIntegration = {
