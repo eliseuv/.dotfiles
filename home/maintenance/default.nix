3530608f0155ecb2cd258339d0b0b24cc0261c31
evf <eliseuv816@gmail.com>
2025-10-13 18:02:32 -0300

    [maintenance] run nix gc weekly

--- home/maintenance/default.nix
+++ home/maintenance/default.nix
@@ -21,7 +21,7 @@
   nix.gc = {
     automatic = true;
     options = "--delete-older-than 7d";
-    dates = "daily";
+    dates = "weekly";
     persistent = true;
     randomizedDelaySec = "45min";
   };
