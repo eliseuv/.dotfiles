0a1acdfc02a89909907403f2b59445d6755a833f
evf <eliseuv816@gmail.com>
2025-10-13 22:23:52 -0300

    [obsidian] use home-manager module

--- home/documents/obsidian.nix
+++ home/documents/obsidian.nix
@@ -1,10 +1,22 @@
-{ pkgs, ... }:
+{ ... }:
 {
 
-  home.packages = with pkgs; [
-
-    obsidian
-
-  ];
+  programs.obsidian = {
+    enable = true;
+    defaultSettings = {
+      app = {
+        "vimMode" = true;
+      };
+      appearance = {
+        "theme" = "obsidian";
+        "cssTheme" = "Minimal";
+      };
+    };
+    vaults = {
+      notes = {
+        target = "Documents/obsidian";
+      };
+    };
+  };
 
 }
