b23fc0bfe30d7842a5cc0d301f72affa62733b03
evf <eliseuv816@gmail.com>
2025-10-15 21:35:04 -0300

    [tardis] comment modules

--- system/hosts/tardis/configuration.nix
+++ system/hosts/tardis/configuration.nix
@@ -7,17 +7,39 @@
 {
 
   imports = [
+
+    # Hardware
     ./hardware.nix
+
+    # Bootloader
     ../../hardware/bootloader.nix
+
+    # Disks
     ../../hardware/disks.nix
+
+    # Audio
     ../../hardware/audio.nix
+
+    # Network
     ../../hardware/network.nix
+
+    # Keyboard
     ../../hardware/keyboard.nix
+
+    # Printing
     ../../hardware/printing.nix
+
+    #Environment
     ../../environment/default.nix
+
+    # Display manager
     ../../desktop/display-manager/gdm.nix
-    ../../desktop/window-manager/gnome.nix
+
+    # Window manager
     ../../desktop/window-manager/hyprland.nix
+    ../../desktop/window-manager/gnome.nix
+
+    # Boot graphics
     ../../extra/plymouth.nix
   ];
 
@@ -27,10 +49,11 @@
     "flakes"
   ];
 
-  nix.settings.trusted-users = [ "evf" ];
-
   # Hostname
   networking.hostName = "tardis";
+
+  # Allow user to install system-wide packages
+  nix.settings.trusted-users = [ "evf" ];
 
   # Disk encryption
   boot.initrd.luks.devices."luks-2ac9cd27-6ff4-4407-9808-c63a5251c44c".device =
@@ -40,8 +63,10 @@
   # services.xserver.libinput.enable = true;
 
   environment.systemPackages = with pkgs; [
+
     # Screen brightness control
     brightnessctl
+
   ];
 
   system.stateVersion = "24.11";
