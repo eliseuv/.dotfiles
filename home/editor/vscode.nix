e64ff9fa7b93265ad6a22a054084f6d956db1139
evf <eliseuv816@gmail.com>
2025-10-30 21:51:35 -0300

    [vscode] add extensions

--- home/editor/vscode.nix
+++ home/editor/vscode.nix
@@ -4,6 +4,20 @@
   programs.vscode = {
 
     enable = true;
+    profiles = {
+      default = {
+        extensions = with pkgs.vscode-extensions; [
+          # Vim emulation
+          vscodevim.vim
+          # Theme
+          catppuccin.catppuccin-vsc
+          catppuccin.catppuccin-vsc-icons
+          # Copilot
+          github.copilot
+          github.copilot-chat
+        ];
+      };
+    };
 
   };
 
