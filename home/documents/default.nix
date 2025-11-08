2f15b472ddb884d5d4007522a2c17c34409f496b
evf <eliseuv816@gmail.com>
2025-11-08 17:50:10 -0300

    [documents] move mime apps config to default module

--- home/documents/default.nix
+++ home/documents/default.nix
@@ -23,4 +23,16 @@
 
   ];
 
+  xdg.mimeApps = {
+    enable = true;
+    associations.added = {
+      "application/pdf" = [ "org.pwmt.zathura-pdf-mupdf.desktop" ];
+      "application/epub" = [ "org.pwmt.zathura-pdf-mupdf.desktop" ];
+    };
+    defaultApplications = {
+      "application/pdf" = [ "org.pwmt.zathura-pdf-mupdf.desktop" ];
+      "application/epub" = [ "org.pwmt.zathura-pdf-mupdf.desktop" ];
+    };
+  };
+
 }
--- home/documents/zathura.nix
+++ home/documents/zathura.nix
@@ -13,16 +13,4 @@
 
   home.sessionVariables.READER = "zathura";
 
-  xdg.mimeApps = {
-    enable = true;
-    associations.added = {
-      "application/pdf" = [ "org.pwmt.zathura-pdf-mupdf.desktop" ];
-      "application/epub" = [ "org.pwmt.zathura-pdf-mupdf.desktop" ];
-    };
-    defaultApplications = {
-      "application/pdf" = [ "org.pwmt.zathura-pdf-mupdf.desktop" ];
-      "application/epub" = [ "org.pwmt.zathura-pdf-mupdf.desktop" ];
-    };
-  };
-
 }
