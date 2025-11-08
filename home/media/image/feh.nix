bcf23088aee75ab1f5394b468430c76bcc558380
evf <eliseuv816@gmail.com>
2025-11-08 17:47:01 -0300

    [feh] use as default image viewer

--- home/media/image/feh.nix
+++ home/media/image/feh.nix
@@ -5,4 +5,16 @@
     enable = true;
   };
 
+  xdg.mimeApps = {
+    enable = true;
+    associations.added = {
+      "image/jpeg" = [ "feh.desktop" ];
+      "image/png" = [ "feh.desktop" ];
+    };
+    defaultApplications = {
+      "image/jpeg" = [ "feh.desktop" ];
+      "image/png" = [ "feh.desktop" ];
+    };
+  };
+
 }
