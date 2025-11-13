ddb25b949e207b5cd6254395437434ea31cc65cd
evf <eliseuv816@gmail.com>
2025-11-13 19:59:47 -0300

    [gemini] override gemini-cli to latest release

--- home/shell/assistant/gemini.nix
+++ home/shell/assistant/gemini.nix
@@ -1,6 +1,30 @@
-{ pkgs-unstable, ... }:
 {
+  pkgs-unstable,
+  lib,
+  ...
+}:
+let
+  gemini-cli-override = pkgs-unstable.gemini-cli.overrideAttrs (old: rec {
+    version = "0.15.0";
+    src = pkgs-unstable.fetchFromGitHub {
+      owner = "google-gemini";
+      repo = "gemini-cli";
+      tag = "v${version}";
+      hash = "sha256-m9NF7O0tmxPOtcoYIb6L658dn3OKpT/RYulZiY64Fms=";
+    };
+    npmDepsHash = "sha256-CyvtMAIWycM0m86GYs6BeQ3EueCk+IluL99/5UaEVr8=";
+    npmDeps = pkgs-unstable.fetchNpmDeps {
+      inherit src;
+      name = "${old.pname}-${version}-npm-deps";
+      hash = npmDepsHash;
+    };
+  });
+in
+{
+  home.packages = [
 
-  home.packages = with pkgs-unstable; [ gemini-cli ];
+    # Custom version of Gemini CLI
+    gemini-cli-override
 
+  ];
 }
