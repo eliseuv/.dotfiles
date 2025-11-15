0b6282a867e3759c52bc442ed512d389b6581df0
evf <eliseuv816@gmail.com>
2025-11-15 20:00:38 -0300

    [gemini] v0.15.4

--- home/shell/assistant/gemini.nix
+++ home/shell/assistant/gemini.nix
@@ -5,14 +5,14 @@
 }:
 let
   gemini-cli-override = pkgs-unstable.gemini-cli.overrideAttrs (old: rec {
-    version = "0.15.0";
+    version = "0.15.4";
     src = pkgs-unstable.fetchFromGitHub {
       owner = "google-gemini";
       repo = "gemini-cli";
       tag = "v${version}";
-      hash = "sha256-m9NF7O0tmxPOtcoYIb6L658dn3OKpT/RYulZiY64Fms=";
+      hash = "sha256-muj8KcF7HKcgkDkjGZhCLzwv5F2gxsO+CvEp3OqhNck=";
     };
-    npmDepsHash = "sha256-CyvtMAIWycM0m86GYs6BeQ3EueCk+IluL99/5UaEVr8=";
+    npmDepsHash = "sha256-6pq0H8fqS6aZNsvKmn28Gn+4PQkVpWz70nS4HKWi5PA=";
     npmDeps = pkgs-unstable.fetchNpmDeps {
       inherit src;
       name = "${old.pname}-${version}-npm-deps";
